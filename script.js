11cce398f649f23a09de70efc032f3a79e89b04a
eduams <esilva36@lenovo.com>
2025-06-30 21:43:48 -0300

    .

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+var total_time = 0;
+
 function myDisplayer(value) {
     console.log(value);
 }
-
+function addTime(time){
+    total_time = total_time + time;
+    document.getElementById("output").innerHTML = "Tempo total de espera: " + total_time/1000 + " segundos";
+}
 const button = document.getElementById('button');
 button.addEventListener('click', waitOrder)
-
 function waitOrder() {
     try{
             const myPromiseInstance = new Promise(function(myResolve, myReject) {
                 console.log("Pedido feito");
+                document.getElementById("output").innerHTML = "Aguardando pedido...";
                 setTimeout(() => {
                     const x = 0;
                     if (x == 0) {
                         myResolve("Pedido pronto");
+                        document.getElementById("output").innerHTML = "Pedido pronto!";
+                        total_time = 0; // Reset total time after order is completed
                     } else {
                         myReject("Error");
                     }
-                }, 10000);
+                }, total_time);
             });
             myPromiseInstance.then(
             function(value) { myDisplayer(value); },
